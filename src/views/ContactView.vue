232c138088f1ca65c252a813cd95da5779331125
LuiV228 <luqmaan.toffey@gmail.com>
2024-01-10 08:43:26 +0200

    folderStructure

---
+++ src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <h1 class="display-2">SMSla ooo kiss to 3-13-14</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
+    <h2 class="display-2">This is the home page</h2>
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style scoped>
+
+</style>
