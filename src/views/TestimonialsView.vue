a734d7535d499f7bbb261100ba895faf226c8785
LuiV228 <luqmaan.toffey@gmail.com>
2024-07-09 11:18:02 +0200

    Project Updates

--- src/views/TestimonialsView.vue
+++ src/views/TestimonialsView.vue
@@ -3,12 +3,11 @@
     <div class="row">
       <h2 class="display-2">Who is Luqmaan?</h2>
     </div>
-    <div class="row m-3">
+    <div class="row" id="card-row">
       <div
         class="col d-flex justify-content-center m-3"
         v-for="testimonial in testimonials"
-        :key="testimonial.name"
-      >
+        :key="testimonial.name">
         <div class="card p-3" style="width: 18rem;">
           <img :src="testimonial.profile" class="card-img-top">
           <div class="card-body">
@@ -16,6 +15,7 @@
             <button 
               type="button" 
               class="btn btn-primary" 
+              id="review"
               data-bs-toggle="modal" 
               data-bs-target="#agentModal" 
               @click="setCurrentTestimonial(testimonial)"
@@ -32,18 +32,17 @@
       <div class="modal-dialog">
         <div class="modal-content loginbg">
           <div class="modal-header">
-            <h1 class="modal-title fs-5" id="agentModalLabel">{{ currentTestimonial.name }} {{ currentTestimonial.surname }} Review:</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h1 class="modal-title fs-5" id="agentModalLabel">{{ currentTestimonial.name }} {{ currentTestimonial.surname }}'s Review:</h1>
           </div>
           <div class="modal-body login-box">
             <div class="user-box">
               <p class="modal-text">{{ currentTestimonial.quotes }}</p>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                Close
-              </button>
-            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" id="close" data-bs-dismiss="modal">
+              Close
+            </button>
           </div>
         </div>
       </div>
@@ -75,45 +74,84 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  background-color: black;
-  color: #18c8e3;
+#card-row {
+  padding-bottom: 30px;
 }
+
+.card {
+  background-color: rgb(114, 119, 255);
+  color: #faf9f6;
+}
+
 .card-text {
   padding: 10px;
   text-align: justify;
 }
-#agentModalLabel {
-  color: #ffffff;
+
+#review {
+  background-color: #36454f;
+  color: #faf9f6;
+  border: 0px;
+  transition: 0.2s;
 }
+
+#review:hover {
+  transform: scale(1.1, 1.1)
+}
+
+#agentModalLabel {
+  color: #faf9f6;
+}
+
 .loginbg {
   margin: 0;
   padding: 0;
-  font-family: sans-serif;
-  background: linear-gradient(#141e30, #243b55);
-  height: 100vh;
+  background: rgb(114, 119, 255);
+  height: 80vh;
 }
+
 .login-box {
   padding: 20px;
-  background: rgba(0, 0, 0, 0.5);
+  background: #36454f;
   box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
   border-radius: 10px;
 }
+
 .modal-header {
   border-bottom: none;
+  padding-left: 20px;
 }
+
 .modal-header .btn-close {
   padding: 1rem;
+  color: #36454f;
 }
+
 .modal-body {
-  max-height: 400px;
+  margin-left: 20px;
+  margin-right: 20px;
+  min-height: 100px;
   overflow-y: auto;
 }
+
 .modal-text {
-  color: #ffffff;
+  color: #faf9f6;
   text-align: justify;
 }
+
 .modal-footer {
   border-top: none;
+  margin-right: 4px;
+}
+
+#close {
+  background-color: #36454f;
+  color: #faf9f6;
+  transition: 0.2s;
+}
+
+#close:hover {
+  box-shadow: 0 0 15px rgb(0, 0, 0, 0.6);
+  transform: scale(1.1, 1.1)
 }
 </style>
